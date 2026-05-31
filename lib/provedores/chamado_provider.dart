41e20c5610ec80c34ed52bf0058082ccf1f7184d
Francisco Serafim <franciscoserafim@MacBook-Air-de-Francisco.local>
2026-05-31 01:26:29 -0300

    agora foi

--- lib/provedores/chamado_provider.dart
+++ lib/provedores/chamado_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 import '../models/chamado.dart';
 import '../database/db_helper.dart';
